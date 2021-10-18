432f9f48f3274e67d077f740b225e7ecf6da33c0
GiordanoSM <giordano@suffert.com>
2021-10-18 06:17:50 -0300

    Exit Menu Done

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -12,4 +12,20 @@ public class ButtonBehavior : MonoBehaviour
     public void ToStudentsMenu(){
         SceneManager.LoadScene(3);
     }
+
+    public string theScene;
+    public void ToExitMenu(){
+        theScene = SceneManager.GetActiveScene().buildIndex.ToString();
+        PlayerPrefs.SetString("LastScene", theScene);
+        SceneManager.LoadScene(4);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+
+    public void CancelExit(){
+        theScene = PlayerPrefs.GetString("LastScene");
+        SceneManager.LoadScene(int.Parse(theScene));
+    }
 }
